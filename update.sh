8f16bca0a6c7f910737c61f727b7d834ae985a41
Krzysztof Mróz <private@mrozo.pl>
2021-01-21 00:33:55 +0100

    porpawka pobierania nowej wersji z git

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git reset HEAD --hard
-git pull
+git fetch --all
+git reset --hard origin/master
 jekyll b
 sudo docker exec -it nextcloudpi rm -rf /var/www/~wydarzenia
-sudo docker exec -it nextcloudpi [ -f /var/www/wydarzenia ] && mv /var/www/wydarzenia /var/www/~wydarzenia                                                                  
+sudo docker exec -it nextcloudpi [ -f /var/www/wydarzenia ] && mv /var/www/wydarzenia /var/www/~wydarzenia
 sudo docker cp ./_site/ nextcloudpi:/var/www/wydarzenia
 

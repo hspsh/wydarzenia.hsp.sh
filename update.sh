90d2100872f7328903b1b607848703d6b955d4f4
Krzysztof Mróz <private@mrozo.pl>
2021-01-21 01:29:22 +0100

    poprawiono niedzialajace kopiowanie plikow do kontenera

--- update.sh
+++ update.sh
@@ -5,5 +5,9 @@ git reset --hard origin/master
 jekyll b
 sudo docker exec -it nextcloudpi rm -rf /var/www/~wydarzenia
 sudo docker exec -it nextcloudpi [ -f /var/www/wydarzenia ] && mv /var/www/wydarzenia /var/www/~wydarzenia
-sudo docker cp ./_site/ nextcloudpi:/var/www/wydarzenia
+sudo docker exec -it nextcloudpi mkdir /var/www/wydarzenia
+
+for f in $(ls _site); do
+	sudo docker cp ./_site/$f nextcloudpi:/var/www/wydarzenia/$f ;
+done
 
